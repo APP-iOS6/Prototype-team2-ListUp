af99e93ef2b562b061a49eb5411508228588bfe5
ActDine <baems0401@naver.com>
2024-08-28 18:08:19 +0900

    MainViewController 수정2
    MainbanerCell URL 변경

--- ListUp/ListUp/ViewControllers/TableCell/MainBanerCell.swift
+++ ListUp/ListUp/ViewControllers/TableCell/MainBanerCell.swift
@@ -32,13 +32,13 @@ class MainBanerCell: UITableViewCell{
     let banners: [UIImage] = [.mainimage1, .mainimage2, .mainimage3, .mainimage4, .mainimage5, .mainimage6, .mainimage7]
     
     let bannerURLs: [URL] = [
-        URL(string: "https://shop-events.interpark.com/exhibition?exhibitionCode=230620008")!,
-        URL(string: "https://commevent.interpark.com/event/338089.do")!,
-        URL(string: "https://mfront.homeplus.co.kr/")!,
-        URL(string: "https://www.coupang.com/")!,
-        URL(string: "https://store.cafe24.com/kr/apps/11813")!,
-        URL(string: "https://www.koreamint.com/main/index.do")!,
-        URL(string: "https://images.homeplus.co.kr/static/renew/event/promotion/130613-stamp/iframe_src.html")!
+        URL(string: "https://flyairseoul.com/CM/ko/eventView.do?seq=1797&type=I")!,
+        URL(string: "https://www.baskinrobbins.co.kr/play/event/view.php?seq=205&category=ALL")!,
+        URL(string: "https://www.shinhansec.com/cms/contents/event/240715_ISAEvent_pc.html")!,
+        URL(string: "https://www.hiddenbay.co.kr/gnu/bbs/board.php?bo_table=event&wr_id=59")!,
+        URL(string: "https://www.suto.co.kr/bbs/board.php?bo_table=cpevent&wr_id=309377")!,
+        URL(string: "https://www.facebook.com/jeonjuncmarket/posts/%EC%97%AC%EB%A6%84%EB%A7%9E%EC%9D%B4-sns%ED%8C%94%EB%A1%9C%EC%9A%B0-%EC%9D%B4%EB%B2%A4%ED%8A%B8-%EC%9D%B4%EB%B2%A4%ED%8A%B8-%EA%B8%B0%EA%B0%84-2024710%EC%88%98-717%EC%88%98-%EC%9D%B4%EB%B2%A4%ED%8A%B8-%EB%8C%80%EC%83%81-%EA%B3%B5%EC%8B%9D-sns-%ED%8C%94%EB%A1%9C%EC%9A%B0-%EA%B3%A0%EA%B0%9D-%EC%9D%B8%EC%8A%A4%ED%83%80%EA%B7%B8%EB%9E%A8%ED%8E%98%EC%9D%B4%EC%8A%A4%EB%B6%81-%EC%B0%B8%EC%97%AC%EB%B0%A9%EB%B2%95-1-%EA%B3%B5%EC%8B%9D-sn/348503028294235/")!,
+        URL(string: "https://www.giantzplay.com/forum/view/1007301")!
     ] // 배너이미지에 대응하는 URL
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
