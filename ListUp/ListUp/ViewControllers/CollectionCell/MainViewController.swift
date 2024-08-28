60c86ce2fcd5337296647ab993f17717b030bccc
ActDine <baems0401@naver.com>
2024-08-28 16:24:03 +0900

    MainViewController 수정
    MainBanerCell 배너별 출력 URL 다르게 수정
    RecommendCell 이미지 별 출력되는 URL 다르게 수정 - 4개당 1개 이미지로 묶여있어 URL이 3종 사용됨

--- ListUp/ListUp/ViewControllers/CollectionCell/MainViewController.swift
+++ ListUp/ListUp/ViewControllers/CollectionCell/MainViewController.swift
@@ -8,8 +8,13 @@
 import UIKit
 import SafariServices
 
-class MainViewController: BaseViewController{
-    let banerImages: [UIImage] = [.mainimage1, .mainimage2, .mainimage3]
+class MainViewController: BaseViewController {
+    
+    let urls: [String] = [
+        "https://shop.interpark.com/malls/index.html?smid1=header&smid2=logo",
+        "https://m.post.naver.com/viewer/postView.naver?volumeNo=29830794&memberNo=30633733&navigationType=push",
+        "https://www.lotteon.com/p/display/main/lotteon"
+    ]
     
     lazy var mainTableView: UITableView = {
         let tableView = UITableView()
@@ -38,25 +43,29 @@ class MainViewController: BaseViewController{
     
     override func setupLayOut() {
         super.setupLayOut()
-    
     }
-    
-    
-    
 }
 
-extension MainViewController: UITableViewDelegate, UITableViewDataSource{
-    
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-        switch indexPath.row{
+        switch indexPath.row {
         case 0:
-            guard let mainBanerCell: MainBanerCell = tableView.dequeueReusableCell(withIdentifier: "MainBanerCell", for: indexPath) as? MainBanerCell else {return UITableViewCell()}
-
+            guard let mainBanerCell: MainBanerCell = tableView.dequeueReusableCell(withIdentifier: "MainBanerCell", for: indexPath) as? MainBanerCell else {
+                return UITableViewCell()
+            }
+            
+            // MainBanerCell의 imageTappedAction 클로저를 설정하여 Safari로 연결
+            mainBanerCell.imageTappedAction = { [weak self] url in
+                let safariViewController = SFSafariViewController(url: url)
+                safariViewController.modalPresentationStyle = .pageSheet
+                self?.present(safariViewController, animated: true)
+            }
+            
             return mainBanerCell
         case 1:
             let sectionCell: SectionCell = SectionCell()
@@ -64,14 +73,15 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource{
             sectionCell.selectionStyle = .none
             return sectionCell
         case 2:
-            guard let shoppingmallListCell: ShoppingmallListCell = tableView.dequeueReusableCell(withIdentifier: "ShoppingmallListCell", for: indexPath) as? ShoppingmallListCell else {return UITableViewCell()}
-
-            shoppingmallListCell.buttonAction = {() -> () in
+            guard let shoppingmallListCell: ShoppingmallListCell = tableView.dequeueReusableCell(withIdentifier: "ShoppingmallListCell", for: indexPath) as? ShoppingmallListCell else {
+                return UITableViewCell()
+            }
+            
+            shoppingmallListCell.buttonAction = { () -> () in
                 if let url = URL(string: "https://www.naver.com") {
-                    let viewController = SFSafariViewController(url: url)
-                    
-                    viewController.modalPresentationStyle = .pageSheet
-                    self.present(viewController, animated: true)
+                    let safariViewController = SFSafariViewController(url: url)
+                    safariViewController.modalPresentationStyle = .pageSheet
+                    self.present(safariViewController, animated: true)
                 }
             }
             return shoppingmallListCell
@@ -81,14 +91,15 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource{
             sectionCell.selectionStyle = .none
             return sectionCell
         case 4:
-            guard let recommendCell: RecommendCell = tableView.dequeueReusableCell(withIdentifier: "RecommendCell", for: indexPath) as? RecommendCell else {return UITableViewCell()}
-
-            recommendCell.tappedAction = {() -> () in
-                if let url = URL(string: "https://www.naver.com") {
-                    let viewController = SFSafariViewController(url: url)
-                    
-                    viewController.modalPresentationStyle = .pageSheet
-                    self.present(viewController, animated: true)
+            guard let recommendCell: RecommendCell = tableView.dequeueReusableCell(withIdentifier: "RecommendCell", for: indexPath) as? RecommendCell else {
+                return UITableViewCell()
+            }
+            
+            recommendCell.tappedAction = { (index: Int) -> () in
+                if let url = URL(string: self.urls[index - 1]) {
+                    let safariViewController = SFSafariViewController(url: url)
+                    safariViewController.modalPresentationStyle = .pageSheet
+                    self.present(safariViewController, animated: true)
                 }
             }
             return recommendCell
@@ -96,10 +107,9 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource{
             return UITableViewCell()
         }
     }
-    
+
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        print("height: \(indexPath)")
-        switch indexPath.row{
+        switch indexPath.row {
         case 0:
             return 300
         case 2:
@@ -110,7 +120,6 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource{
             return 30
         }
     }
-    
 }
 
 
